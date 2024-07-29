a223fc49e3d33eafa33b1f8e5acf1337164fe663
9matt <76255461+9matt@users.noreply.github.com>
2024-07-29 20:38:36 +0200

    rewrite main loop as function

--- video picker.py
+++ video picker.py
@@ -10,41 +10,31 @@ from beatstars_config import Videopicker
 def listdir_nohidden(path):
     return glob.glob(os.path.join(path, '*'))
 
-# videos per one clip
-video_count = 5
-
 pack_name = input('What is the pack name? ')
-viddir = input('Input the video directory path: ')
-viddir = viddir.strip("'\"")
-
+viddir = Videopicker.video_folder.strip("'\"")
+outputs = int(input('How many videos should be rendered? '))
 pack_path = Path(f'{Videopicker.packs_folder}/{pack_name}')
 finals_path = Path(f'{Videopicker.packs_folder}/{pack_name}/vids')
-if not pack_path.exists():
-    print("Warning: the pack folder doesn't seem to exist")
-    output_count = int(input('How many videos should be rendered? '))
-else:
-    output_count = 0
-    for temps in listdir_nohidden(Path(f'{Videopicker.packs_folder}/{pack_name}/temps')):
-        output_count += 1
-
 finals_path.mkdir(parents=True)
 
-# main loop
-video_list = []
-for item in listdir_nohidden(viddir):
-    video_list.append(item)
+def generator(video_directory, output_count, render_path, video_count):
+    video_list = []
+    for item in listdir_nohidden(video_directory):
+        video_list.append(item)
 
-output_number = 1
-for i in range(output_count):
-    args = str('')
-    for i in range(video_count):
-        num = random.randint(0, len(video_list))
-        os.chdir(finals_path)
-        f = open("vidlist.txt", "a+") 
-        f.write(f'file {video_list[num]}\n')
-        f.close()
+    output_number = 1
+    for i in range(output_count):
+        args = str('')
+        for i in range(video_count):
+            num = random.randint(0, len(video_list))
+            os.chdir(render_path)
+            f = open("vidlist.txt", "a+") 
+            f.write(f'file {video_list[num]}\n')
+            f.close()
 
-    render_ffmpeg = str(f'ffmpeg -f concat -safe 0  -i vidlist.txt -c copy "{finals_path}/{output_number}.mp4"')
-    subprocess.run(render_ffmpeg, shell = True, executable="/bin/bash")
-    output_number += 1
-    os.remove('vidlist.txt')
+        render_ffmpeg = str(f'ffmpeg -f concat -safe 0  -i vidlist.txt -c copy "{render_path}/{output_number}.mp4"')
+        subprocess.run(render_ffmpeg, shell = True, executable="/bin/bash")
+        output_number += 1
+        os.remove('vidlist.txt')
+
+generator(viddir, outputs, finals_path, 5)
