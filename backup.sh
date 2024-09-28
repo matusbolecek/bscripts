38b795f53b186fc1006612c5fabb4c7f7cc2ddcd
9matt <76255461+9matt@users.noreply.github.com>
2024-09-28 14:11:31 +0200

    backup.sh no longer necessary

--- backup.sh
+++
@@ -1,6 +0,0 @@
-rm -rf Documents/com~apple~CloudDocs/FL/Bounces/*
-cp -v -n -r ~/BEATSTARS/Bounces/* /Users/matusbolecek/Library/Mobile\ Documents/com~apple~CloudDocs/FL/Bounces/
-cp -v -r -n /Volumes/wd/BEATS/* /Users/matusbolecek/Library/Mobile\ Documents/com~apple~CloudDocs/FL/Projects/
-
-current_date=$(date +"%Y-%m-%d")
-echo "Backup: $current_date" >> backups.txt
